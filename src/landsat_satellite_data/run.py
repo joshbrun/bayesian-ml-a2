eccacda3de4b1f2cc6d9619b56264693f0badcf2
joshbrun <josh.brundan@gmail.com>
2018-09-18 13:19:01 +1200

    initialise directory hierarchy

---
+++ src/landsat_satellite_data/run.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+"""
+SE755 - A2
+Data set: 3 - Landsat Satellite Data
+ML type: Classification
+
+Algorithms: logistic_regression, deep_learning, bayesian_deep_learning
+Runs the training and analysis of the above three classification algorithms
+
+Authors:
+Joshua Brundan
+Kevin Hira
+"""
+
---
+++ src/occupancy_sensor_data/run.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+"""
+SE755 - A2
+Data set: 2 - Occupancy Sensor Data
+ML type: Clustering
+
+Algorithms: k_means, gaussian_mixture_model
+Runs the training and analysis of the above two clustering algorithms
+
+Authors:
+Joshua Brundan
+Kevin Hira
+"""
+
+
